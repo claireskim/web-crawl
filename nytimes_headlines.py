614d83fbc2ec9e69e0a8f19d9a19b4d9c3b65a0d
Claire <49087389+claireskim@users.noreply.github.com>
2019-07-03 14:44:20 -0700

    Update nytimes_headlines.py

--- nytimes_headlines.py
+++ nytimes_headlines.py
@@ -1,1 +1,18 @@
-# web-crawl
+# Scrape the front page of NY Times for the headlines
+# Uses BeautifulSoup
+# JS version coming
+
+import requests
+from bs4 import BeautifulSoup
+
+url = 'http://www.nytimes.com'
+r = requests.get(url)
+soup = BeautifulSoup(r.text)
+headings = []
+
+title = soup.findAll('h2', {'class': 'css-1qwxefa'})
+
+for row in title:
+     headings.append(row.text)
+
+print(headings)
